732eaf284ce19c7db61673602e4433879f8ca69d
김현호의 맥미니 <kimhyunhomacmini@gimhyeonhouiui-Macmini.local>
2026-02-10 16:59:10 +0900

    Web: rebuild app bundle (recommendations strict validate)

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c3b2d40f28bc47d5f803ddc3956c1084",
+const RESOURCES = {"flutter_bootstrap.js": "037eb6f36ea3b857aadfc66382d6a13e",
 "version.json": "9e55a305267d779c535043c4f308b940",
 "index.html": "e42ed36cfb12451bb435be9b70c1799f",
 "/": "e42ed36cfb12451bb435be9b70c1799f",
-"main.dart.js": "b60623f09251210d2f5994747b997041",
+"main.dart.js": "c6d25832c3344508802211e267c34c01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
