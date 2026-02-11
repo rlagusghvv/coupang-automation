19d0f272001e4fac79d56446ad9d5d04745c6bc6
김현호의 맥미니 <kimhyunhomacmini@gimhyeonhouiui-Macmini.local>
2026-02-11 09:54:37 +0900

    Web: rebuild Flutter web bundle (tab persistence + bg reco)

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f0dc4e6404856845de08fdcc9d3cb0a8",
+const RESOURCES = {"flutter_bootstrap.js": "71d898295f3b9b68f22a7e92c3e0e103",
 "version.json": "c966ae4c22b14129175e30a1991dc1af",
 "index.html": "e42ed36cfb12451bb435be9b70c1799f",
 "/": "e42ed36cfb12451bb435be9b70c1799f",
-"main.dart.js": "34ad14989ceb05bf0adf0c1d8e1511d8",
+"main.dart.js": "a14e32329c852daa261f08c14569b1ff",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
