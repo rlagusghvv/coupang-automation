7a95f10581990ef8fa45e8a52524ff03d8fa0cfb
김현호의 맥미니 <kimhyunhomacmini@gimhyeonhouiui-Macmini.local>
2026-02-11 09:03:41 +0900

    Web: rebuild Flutter web bundle (batch queue UI)

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e51af137ec70c756defdec14f762be5f",
+const RESOURCES = {"flutter_bootstrap.js": "77829bdf38fc31927fcc53559200366a",
 "version.json": "c966ae4c22b14129175e30a1991dc1af",
 "index.html": "98756a5b6514a02f8d3478bab94c75d0",
 "/": "98756a5b6514a02f8d3478bab94c75d0",
-"main.dart.js": "875bfae6d7005c55b77f1dbb86667099",
+"main.dart.js": "b0f34ee6bd119e5fc9746a5267ab7d5f",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter_bootstrap.js": "e51af137ec70c756defdec14f762be5f",
 "assets/lib/assets/splash/elephant_splash.png": "64bb50f8c69a4c35cd34e607f88eb6b7",
 "assets/lib/assets/icon/elephant_1024.png": "4fae03846dbb14a961f186e60219c6ee",
 "assets/AssetManifest.bin": "40e736aeaa43d6d0ecd8b1b139a57ee4",
-"assets/fonts/MaterialIcons-Regular.otf": "72fd8210739024355b94460b1d977485",
+"assets/fonts/MaterialIcons-Regular.otf": "2997b4d33d1c7081e9bb0033ba2067c6",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
